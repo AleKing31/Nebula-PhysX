11f090309a641c25e9f0985f464d2cbb2f507851
Charles Rymal <charlesrymal@gmail.com>
2014-09-05 08:58:35 -0700

    adding light shadow environ frustrum overlap check

---
+++ include/neb/phx/test.hpp
@@ -0,0 +1,12 @@
+#ifndef NEB_PHX_TEST
+#define NEB_PHX_TEST
+
+
+namespace neb {
+	physx::PxConvexMeshGeometry	frustrum_geometry(glm::mat4 proj);
+	bool				query(physx::PxConvexMeshGeometry& g0, glm::mat4 v0, physx::PxConvexMeshGeometry& g1, glm::mat4 v1);
+	bool				frustrum_overlap(neb::gfx::environ::base * const e0, neb::gfx::environ::base * const e1);
+}
+
+
+#endif
