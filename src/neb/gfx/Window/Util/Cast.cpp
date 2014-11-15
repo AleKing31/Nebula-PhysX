2438861c9f456641e4eab0008767731b06e296f8
chuck <charlesrymal@gmail.com>
2014-11-15 12:03:55 -0800

    auto

--- src/neb/gfx/Window/Util/Cast.cpp
+++
@@ -1,7 +0,0 @@
-#include <neb/gfx/window/util/Cast.hh>
-#include <neb/gfx/window/Base.hh>
-
-std::shared_ptr<neb::gfx::window::base>	neb::gfx::window::util::cast::isWindowBase() {
-	return std::dynamic_pointer_cast<neb::gfx::window::base>(shared_from_this());
-}
-
