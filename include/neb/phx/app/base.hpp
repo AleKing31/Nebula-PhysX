42433c5c862d9588ad8a267c75827bf5b019ac83
chuck <charlesrymal@gmail.com>
2015-02-04 22:21:07 -0800

    ?

--- include/neb/phx/app/base.hpp
+++ include/neb/phx/app/base.hpp
@@ -5,9 +5,9 @@
 
 #include <PxPhysicsAPI.h>
 
-#include <neb/core/util/config.hpp>
-#include <neb/core/app/__core.hpp>
-#include <neb/core/core/scene/base.hpp>
+#include <neb/core/app/__base.hpp>
+#include <neb/core/util/config.hpp> // neb/core/util/config.hpp.in
+//#include <neb/core/core/scene/base.hpp>
 
 #include <neb/phx/core/scene/util/parent.hpp>
 //#include <neb/phx/game/game/util/parent.hpp>
@@ -30,38 +30,31 @@ physx::PxFilterFlags DefaultFilterShader(
 		physx::PxFilterData filterData1,
 		physx::PxPairFlags& pairFlags,
 		const void* constantBlock,
-		physx::PxU32 constantBlockSize );
+		physx::PxU32 constantBlockSize);
 
-namespace neb {
-	namespace phx {
-		namespace app {
-
-
-			class base:
-				virtual public nc::app::base,
-				virtual public neb::phx::core::scene::util::parent
-				//virtual public neb::phx::game::game::util::parent
-			{
-				public:
-					static std::shared_ptr<phx::app::base>		global();
-					base();
-				protected:
-					void						__init();
-				public:
-					void						release();
-					virtual void					step(gal::etc::timestep const & ts);
-					DefaultErrorCallback 				px_default_error_callback_;
-					physx::PxDefaultAllocator 			px_default_allocator_callback_;
-					physx::PxFoundation*				px_foundation_;
-					physx::PxPhysics*				px_physics_;
-					physx::PxProfileZoneManager*			px_profile_zone_manager_;
-					physx::PxCooking*				px_cooking_;
-					//physx::pxtask::CudaContextManager*		px_cuda_context_manager_;
-					physx::PxControllerManager*			px_character_controller_manager_;
-			};
-		}
-	}
-}
-
+namespace neb { namespace phx { namespace app {
+	class base:
+		virtual public neb::core::app::base,
+		virtual public neb::phx::core::scene::util::parent
+		//virtual public neb::phx::game::game::util::parent
+	{
+		public:
+			static std::shared_ptr<phx::app::base>		global();
+			base();
+		protected:
+			void						__init();
+		public:
+			void						release();
+			virtual void					step(gal::etc::timestep const & ts);
+			DefaultErrorCallback 				px_default_error_callback_;
+			physx::PxDefaultAllocator 			px_default_allocator_callback_;
+			physx::PxFoundation*				px_foundation_;
+			physx::PxPhysics*				px_physics_;
+			physx::PxProfileZoneManager*			px_profile_zone_manager_;
+			physx::PxCooking*				px_cooking_;
+			//physx::pxtask::CudaContextManager*		px_cuda_context_manager_;
+			physx::PxControllerManager*			px_character_controller_manager_;
+	};
+}}}
 
 #endif
